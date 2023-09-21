99013c52154c677ab9f11ca44b89435533afd87c
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-09-21 15:41:01 +0300

    add:styles Selects.tsx

--- src/components/Selects/Selects.styles.ts
+++ src/components/Selects/Selects.styles.ts
@@ -6,3 +6,34 @@ export const SelectsWrap = styled.div`
   gap: 18px;
   margin-bottom: 50px;
 `;
+export const SelectBlocWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+`;
+
+export const SelectBlocTitle = styled.p`
+  margin-bottom: 8px;
+  color: #8a8a89;
+  font-family: Manrope;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const FormInputFrom = styled.input`
+  padding: 14px 93px 14px 24px;
+  border-radius: 14px 0px 0px 14px;
+  border-right: 1px solid rgba(138, 138, 137, 0.2);
+  border-style: none;
+  background: #f7f7fb;
+`;
+
+export const FormInputTo = styled.input`
+  padding: 14px 93px 14px 24px;
+  border-radius: 0px 14px 14px 0px;
+  border-right: 1px solid rgba(138, 138, 137, 0.2);
+  border-style: none;
+  background: #f7f7fb;
+`;
--- src/components/Selects/Selects.tsx
+++ src/components/Selects/Selects.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import Select from "react-select";
-import { SelectsWrap } from "./Selects.styles";
+import {
+  FormInput,
+  FormInputFrom,
+  FormInputTo,
+  SelectBlocTitle,
+  SelectBlocWrap,
+  SelectsWrap,
+} from "./Selects.styles";
 import makes from "../../data/makes.json";
 
 type Props = {
@@ -26,31 +33,103 @@ function Selects({
   }));
   return (
     <SelectsWrap>
-      <Select
-        options={makesOptions}
-        onChange={(option) => selectedCarsByMark(option?.label)}
-        isClearable={isClearable}
-        name="mark"
-      />
-      <Select
-        options={priceOptions}
-        onChange={(option) => selectedCarsByPrice(option?.label)}
-        isClearable={isClearable}
-        name="price"
-      />
-      <form
-        onSubmit={selectedCarsByMileage}
-        name="search_form"
-        autoComplete="on"
-        noValidate
-      >
-        <label>
-          Сar mileage / km
-          <input type="text" name="startMileage" />
-          <input type="text" name="endMileage" />
-        </label>
-        <button type="submit">Search</button>
-      </form>
+      <SelectBlocWrap>
+        <SelectBlocTitle>Car brand</SelectBlocTitle>
+        <Select
+          options={makesOptions}
+          onChange={(option) => selectedCarsByMark(option?.label)}
+          isClearable={isClearable}
+          name="mark"
+          placeholder="Enter the text"
+          styles={{
+            control: (base) => ({
+              ...base,
+              paddingTop: "14px",
+              paddingLeft: "18px",
+              paddingBottom: "14px",
+              borderRadius: 14,
+              background: "#F7F7FB",
+              borderStyle: "none",
+              borderColor: "#fff",
+              boxShadow: "#fff",
+              ":hover": {
+                ...base[":hover"],
+                borderColor: "#fff",
+                boxShadow: "#fff",
+              },
+            }),
+            placeholder: (base) => ({
+              ...base,
+              color: "#121417",
+              fontFamily: "Manrope",
+              fontSize: 18,
+              fontStyle: "normal",
+              fontWeight: 500,
+            }),
+            indicatorsContainer: (base) => ({
+              ...base,
+              marginLeft: 51,
+              marginRight: 18,
+            }),
+          }}
+        />
+      </SelectBlocWrap>
+      <SelectBlocWrap>
+        <SelectBlocTitle>Price/ 1 hour</SelectBlocTitle>
+        <Select
+          options={priceOptions}
+          onChange={(option) => selectedCarsByPrice(option?.label)}
+          isClearable={isClearable}
+          name="price"
+          placeholder="To $"
+          styles={{
+            control: (base) => ({
+              ...base,
+              paddingTop: "14px",
+              paddingLeft: "18px",
+              paddingBottom: "14px",
+              borderRadius: 14,
+              background: "#F7F7FB",
+              borderStyle: "none",
+              borderColor: "#fff",
+              boxShadow: "#fff",
+              ":hover": {
+                ...base[":hover"],
+                borderColor: "#fff",
+                boxShadow: "#fff",
+              },
+            }),
+            placeholder: (base) => ({
+              ...base,
+              color: "#121417",
+              fontFamily: "Manrope",
+              fontSize: 18,
+              fontStyle: "normal",
+              fontWeight: 500,
+            }),
+            indicatorsContainer: (base) => ({
+              ...base,
+              marginLeft: 51,
+              marginRight: 18,
+            }),
+          }}
+        />
+      </SelectBlocWrap>
+      <SelectBlocWrap>
+        <SelectBlocTitle>Сar mileage / km</SelectBlocTitle>
+        <form
+          onSubmit={selectedCarsByMileage}
+          name="search_form"
+          autoComplete="on"
+          noValidate
+        >
+          <label>
+            <FormInputFrom type="text" name="startMileage" placeholder="From" />
+            <FormInputTo type="text" name="endMileage" placeholder="To" />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </SelectBlocWrap>
     </SelectsWrap>
   );
 }
