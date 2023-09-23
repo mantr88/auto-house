bd68eb045229aab63a8250a8d4f89f3dd2c7de36
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-09-23 20:57:29 +0300

    update logic fetched cars in favorites page

--- src/components/FavoritesList/FavoritesList.tsx
+++ src/components/FavoritesList/FavoritesList.tsx
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 import { Cars } from "../../ui/CardList/CardList.types";
-import Card from "../../ui/Card/Card";
-import { ListContainer } from "../../ui/CardList/CardList.styled";
 import NotFound from "../../ui/Not Found/NotFound";
+import CardList from "../../ui/CardList/CardList";
 
 function FavoritesList() {
   const [cars, setCars] = useState<Cars | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const favoritesItems: string | null = localStorage.getItem(
-      "favoriteCars"
-    ) as string;
-    const favoritesItemsParse = JSON.parse(favoritesItems);
-    if (favoritesItemsParse) {
-      setCars((prevState) => [...prevState, ...favoritesItemsParse]);
+    try {
+      setIsLoading(true);
+      const favoritesItems: string | null = localStorage.getItem(
+        "favoriteCars"
+      ) as string;
+      const favoritesItemsParse = JSON.parse(favoritesItems);
+      if (favoritesItemsParse) {
+        setCars((prevState) => [...prevState, ...favoritesItemsParse]);
+      }
+    } catch (error) {
+      throw new Error(`😢Sorry, it is error. Your error 👉 ${error}`);
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
   return (
-    <ListContainer>
+    <main>
       {cars.length === 0 && <NotFound />}
-      {cars.map((car, idx) => (
-        <Card key={idx} car={car}></Card>
-      ))}
-    </ListContainer>
+      <CardList cars={cars} isLoading={isLoading} />
+    </main>
   );
 }
 
